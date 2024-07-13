a80187f5e19881572e4ef3c77e11c580b33cd91f
kevin0395 <kevincorazza09@gmail.com>
2024-07-13 22:46:48 +0200

    first commit

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-document.querySelectorAll('.preview').forEach(preview => {
-  const url = preview.getAttribute('data-url');
-  const iframe = document.createElement('iframe');
-  iframe.title = "Speckle";
-  iframe.src = url;
-  iframe.width = "100%";
-  iframe.height = "100%";
-  iframe.frameBorder = "0";
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll('.preview').forEach(preview => {
+    const url = preview.getAttribute('data-url');
+    const iframe = document.createElement('iframe');
+    iframe.title = "Speckle";
+    iframe.src = url;
+    iframe.width = "100%";
+    iframe.height = "100%";
+    iframe.frameBorder = "0";
 
-  preview.querySelector('.preview-iframe-container').appendChild(iframe);
-  preview.addEventListener('click', () => {
-    window.location.href = url;
+    preview.querySelector('.preview-iframe-container').appendChild(iframe);
+    preview.addEventListener('click', () => {
+      window.location.href = url;
+    });
   });
 });
